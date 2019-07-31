775e2c9b81a135def2b18ddc078f8dd607cd9d2d
oh Kay <khtdr.com@gmail.com>
2019-07-30 21:59:35 -0600

    working ebnf-mode

---
+++ ebnf-mode.el
@@ -0,0 +1,21 @@
+;;; ebnf-mode --- Generic mode for *.ebnf files
+;;; Commentary:
+
+;;; Code:
+(define-generic-mode 'ebnf-mode
+  '(("(*" . "*)"))
+  '()
+  '(("^[^ \t\r\n]+[^\r\n=]*" . font-lock-type-face)
+    ("\\?.*\\?" . font-lock-doc-face)
+    ("[{}()[]\\|\\]" . font-lock-negation-char-face)
+    ("[,|]" . font-lock-builtin-face)
+    ("[=;]" . font-lock-builtin-face)
+    ("[^ \t\n]" . font-lock-keyword-face))
+  '("\\.ebnf\\'")
+  '((lambda ()
+      (modify-syntax-entry ?' "\"")
+      (setq mode-name "EBNF")))
+  "Major mode for EBNF metasyntax text highlighting.")
+
+(provide 'ebnf-mode)
+;;; ebnf-mode ends here
